22659ba66b36fe75c6affd20744bf05d67543d86
konstantinos193 <konstantinosblavakis@gmail.com>
2025-10-15 00:57:13 +0300

    Fix redis import path in blog-optimized.js - change from ../config/redis to ../config/upstash-redis

--- routes/blog-optimized.js
+++ routes/blog-optimized.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const Blog = require('../models/Blog');
-const { cache } = require('../config/redis');
+const { cache } = require('../config/upstash-redis');
 
 // Cache duration constants
 const CACHE_DURATION = {
