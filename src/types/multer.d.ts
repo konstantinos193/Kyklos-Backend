65c708b971c8d2463bcd1613df493f2cc4a29a9f
konstantinos193 <konstantinosblavakis@gmail.com>
2025-11-12 17:50:44 +0200

    build issues fix

--- src/types/multer.d.ts
+++ src/types/multer.d.ts
@@ -1,4 +1,28 @@
-import 'multer';
+import { Readable } from 'stream';
+
+declare global {
+  namespace Express {
+    namespace Multer {
+      interface File {
+        fieldname: string;
+        originalname: string;
+        encoding: string;
+        mimetype: string;
+        size: number;
+        stream: Readable;
+        destination?: string;
+        filename?: string;
+        path?: string;
+        buffer?: Buffer;
+      }
+    }
+
+    interface Request {
+      file?: Multer.File;
+      files?: Multer.File[] | Record<string, Multer.File[]>;
+    }
+  }
+}
 
 export {};
 
